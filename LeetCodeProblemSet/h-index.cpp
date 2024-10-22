9680dd2ffd2f376f03459bbf4e1ed8a7892c2692
Daniel Lawand <daniel.lawand@gmail.com>
2024-10-22 17:48:59 -0300

    WIP

---
+++ LeetCodeProblemSet/h-index.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        size_t n = citations.size();
+        vector<int> val(n+1);
+        int i = n-1;
+        int aux = n-1;
+        for (int j = 0; j < n; j++) {val[j] = 0;}
+        int pos;
+
+        while (i >= 0 ) {
+            pos = aux;
+            if (citations[i] >= aux+1) {
+                val[aux]++;
+                i--;
+            } 
+            
+            if (val[pos] >= pos+1) {
+                return pos+1;
+            }
+            if (citations[i] < aux+1) {
+                if (aux == 0) {return 0;}
+                val[aux-1] = val[aux];
+                aux--;
+            }
+        }
+        return 0;
+    }
+};
+
+int main() {
+    Solution solution;
+    int i = 0, expected;
+
+    vector<int> v = {3,0,6,1,5};
+    expected = 3;
+    cout << "Teste: " << i << endl;
+    for (const int& number : v) {
+        cout << number << " ";
+    }
+    cout << endl;
+    cout << "Answer: "<< solution.hIndex(v) << " -- Expected: " << expected << endl;
+
+    i++;
+    v = {1,3,1};
+    expected = 1;
+    cout << "Teste: " << i << endl;
+    for (const int& number : v) {
+        cout << number << " ";
+    }
+    cout << endl;
+    cout << "Answer: "<< solution.hIndex(v) << " -- Expected: " << expected << endl;
+
+    i++;
+    v = {0};
+    expected = 0;
+    cout << "Teste: " << i << endl;
+    for (const int& number : v) {
+        cout << number << " ";
+    }
+    cout << endl;
+    cout << "Answer: "<< solution.hIndex(v) << " -- Expected: " << expected << endl;
+}
