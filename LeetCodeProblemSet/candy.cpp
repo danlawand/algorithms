3fb3f96dbc2f16aa3f806c92422affa27b74597f
Daniel Lawand <daniel.lawand@gmail.com>
2025-01-07 18:34:10 -0300

    wip candy

--- LeetCodeProblemSet/candy.cpp
+++ LeetCodeProblemSet/candy.cpp
@@ -7,36 +7,58 @@ class Solution {
 public:
     int candy(vector<int>& r) {
         int n = static_cast<int>(r.size());
+        vector<int> ca;
         int c = 0, ant = 0, q = 0;
-        for (int i = 1; i < n-1; i++) {
-            if (r[i] == r[i-1]) { 
-                c++; 
-                ant = r[i];
-                q = 1;
-            }
-            else if (r[i] < r[i-1]) {
-                if (r[i] > ant) {
-                    q++;
-                    c += q;
-                }
-                else {
+        int i = 0;
+        int e, m, d;
+        int passo = 1;
 
-                }
-            }
-            else {
-                c
+        for (i = 1; i < n-1; i++) {
+            e = r[i-1];
+            m = r[i];
+            d = r[i+1];
+
+            if (e > m && m > d) {
+                
             }
 
-
+            // if (r[i] > r[i+1]) {
+            //     ca[i]++;
+            // } else if (r[i] < r[i+1]) {
+            //     if (ca[i] >= ca[i+1]) {
+            //         ca[i+1] = ca[i] + 1;
+            //     }
+            // }
         }
+        return c;
     }
 };
 
 
 int main() {
     Solution sol = Solution();
-    vector<int> r = {1,0,2};
-    int e = 5; // 2 1 2
+    vector<int> r = {0,1,2};
+    int e = 6; // 1 2 3
+    cout << "Expected: " << e << " -> Actual: " << sol.candy(r) << endl;
+
+    r = {0};
+    e = 1; // 1
+    cout << "Expected: " << e << " -> Actual: " << sol.candy(r) << endl;
+
+    r = {1, 0};
+    e = 3; // 2 1
+    cout << "Expected: " << e << " -> Actual: " << sol.candy(r) << endl;
+
+    r = {5, 5};
+    e = 2; // 1 1
+    cout << "Expected: " << e << " -> Actual: " << sol.candy(r) << endl;
+
+    r = {2,1,0};
+    e = 6; // 3 2 1
+    cout << "Expected: " << e << " -> Actual: " << sol.candy(r) << endl;
+
+    r = {1,0,2};
+    e = 5; // 2 1 2
     cout << "Expected: " << e << " -> Actual: " << sol.candy(r) << endl;
     
     r = {1,2,2};
